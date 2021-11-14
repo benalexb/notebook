ab6406488c262ff74c18b8048fe6bfddc7331e24
Benjamin A. de Macedo Barreto <benalexb@gmail.com>
2021-11-14 15:08:01 +0100

    Finished binarySearch exercise

---
+++ exercises/search/binarySearch.js
@@ -0,0 +1,35 @@
+// Write a function called binarySearch which accepts a sorted array and a value, and returns the
+// index at which the value exists. Otherwise, return -1.
+
+const binarySearch = (numberList, searchValue) => {
+  // The idea of binary search comes from Divide and Conquer, assuming a sorted array of numbers.
+  // Using leftPointer and rightPointer, we mark the beginning and the end of the list.
+  // Looping, take the center between pointers and check the current value against the search value.
+  // The center point is the average between points, rounded up.
+  // If the current value is greater than the search value, leftPointer moves to the current pos.
+  // if the current value is less than the search value, rightPointer moves to the current pos.
+  // The loop breaks when the value has been found, or the pointers are equal.
+  if (!numberList || numberList.length === 0) return -1;
+
+  let leftPointer = 0;
+  let rightPointer = numberList.length - 1;
+
+  while (leftPointer < rightPointer) {
+    const searchPoint = Math.ceil((rightPointer + leftPointer) / 2);
+    const currentValue = numberList[searchPoint];
+    if (currentValue > searchValue) {
+      rightPointer = searchPoint;
+    } else if (currentValue < searchValue) {
+      leftPointer = searchPoint;
+    } else {
+      // currentValue must be equal to searchValue
+      return searchPoint;
+    }
+  }
+
+  return -1;
+};
+
+binarySearch([1, 2, 3, 4, 5], 6);
+
+module.exports = { binarySearch };
---
+++ exercises/search/binarySearch.test.js
@@ -0,0 +1,25 @@
+const { binarySearch } = require('./binarySearch');
+
+describe('binarySearch', () => {
+  it('should return -1 when the passed value is not in the array', () => {
+    expect(binarySearch([1, 2, 3, 4, 5], 6)).toBe(-1);
+    expect(binarySearch(
+      [5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],
+      100,
+    )).toBe(-1);
+  });
+
+  it('should return the index at which the value is located', () => {
+    expect(binarySearch([1, 2, 3, 4, 5], 2)).toBe(1);
+    expect(binarySearch([1, 2, 3, 4, 5], 3)).toBe(2);
+    expect(binarySearch([1, 2, 3, 4, 5], 5)).toBe(4);
+    expect(binarySearch(
+      [5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],
+      10,
+    )).toBe(2);
+    expect(binarySearch(
+      [5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],
+      95,
+    )).toBe(16);
+  });
+});
